f7d7432fa0c6394d39cafb23c84fc5bf7d24fc8b
Ohad Cohen <ohad909881122@gmail.com>
2023-01-18 12:31:43 +0200

    U

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -5,52 +5,52 @@ import { TypingText } from "../CustomTexts";
 import styles from "../../styles";
 import "./testimonials.css";
 
-import { Swiper } from "swiper/react";
+// import { Swiper } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
-import { Pagination } from "swiper";
+// import { Pagination } from "swiper";
 
-const data = [
-  {
-    avatar1: "/Eliyahu.jpeg",
-    name: "Eliyahu Yifrach",
-    title: "Founder | CEO of Roommie",
-    review: "",
-  },
-  {
-    avatar2: "/Ohad.jpeg",
-    name: "Ohad Cohen",
-    title: "CTO | Fullstack Developer",
-    review: "",
-  },
-  {
-    avatar3: "/Yonatan.jpeg",
-    name: "Yonatan Yavin",
-    title: "Backend Developer",
-    review:
-      "Computer Engineering graduate with strong skills in C, C++, Java, Python. Specializing in embedded systems and software engineering",
-  },
-  {
-    avatar4: "/Liat.jpeg",
-    name: "Liat Shear",
-    title: "Frontend Developer",
-    review: "Computer Science Student",
-  },
-  {
-    avatar5: "/Omer.jpeg",
-    name: "Omer Dayan",
-    title: "Backend Developer",
-    review: `Over the past few years, I've learned a lot about programming in C++ and Python.
-        I carried out interesting projects, I gave programming lessons to students and friends.`,
-  },
-  {
-    avatar6: "/Omer.jpeg",
-    name: "Yael Gargir",
-    title: "CMO",
-    review: `Over the past few years, I've learned a lot about programming in C++ and Python.
-        I carried out interesting projects, I gave programming lessons to students and friends.`,
-  },
-];
+// const data = [
+//   {
+//     avatar1: "/Eliyahu.jpeg",
+//     name: "Eliyahu Yifrach",
+//     title: "Founder | CEO of Roommie",
+//     review: "",
+//   },
+//   {
+//     avatar2: "/Ohad.jpeg",
+//     name: "Ohad Cohen",
+//     title: "CTO | Fullstack Developer",
+//     review: "",
+//   },
+//   {
+//     avatar3: "/Yonatan.jpeg",
+//     name: "Yonatan Yavin",
+//     title: "Backend Developer",
+//     review:
+//       "Computer Engineering graduate with strong skills in C, C++, Java, Python. Specializing in embedded systems and software engineering",
+//   },
+//   {
+//     avatar4: "/Liat.jpeg",
+//     name: "Liat Shear",
+//     title: "Frontend Developer",
+//     review: "Computer Science Student",
+//   },
+//   {
+//     avatar5: "/Omer.jpeg",
+//     name: "Omer Dayan",
+//     title: "Backend Developer",
+//     review: `Over the past few years, I've learned a lot about programming in C++ and Python.
+//         I carried out interesting projects, I gave programming lessons to students and friends.`,
+//   },
+//   {
+//     avatar6: "/Omer.jpeg",
+//     name: "Yael Gargir",
+//     title: "CMO",
+//     review: `Over the past few years, I've learned a lot about programming in C++ and Python.
+//         I carried out interesting projects, I gave programming lessons to students and friends.`,
+//   },
+// ];
 
 function Testimonials() {
   return (

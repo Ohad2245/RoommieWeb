5165dd64c22c87f86cd818ff52e48d2fcf31d938
Ohad Cohen <ohad909881122@gmail.com>
2023-01-24 13:40:43 +0200

    U

--- src/components/why/Why2.js
+++ src/components/why/Why2.js
@@ -4,47 +4,51 @@ import "./why2.css";
 
 const Why2 = () => {
   return (
-      <div class="container experience__container reveal">
-          <div class="experience__content">
-            <article className="experience__details ">
-              {/* <BsPatchCheckFill className="experience__details-icon" /> */}
-              <div className="text-white">
-                <li>Reduce frustration</li>
-              </div>
-            </article>
-            <article className="experience__details">
-              {/* <BsPatchCheckFill className="experience__details-icon" /> */}
-              <div className="text-white">
-                <li>Save time and energy exerted in searching</li>
-              </div>
-            </article>
-            <article className="experience__details">
-              {/* <BsPatchCheckFill className="experience__details-icon" /> */}
-              <div className="text-white">
-                <li>Reduce gap between what people want vs. what they settle for</li>
-              </div>
-            </article>
-            <article className="experience__details">
-              {/* <BsPatchCheckFill className="experience__details-icon" /> */}
-              <div className="text-white">
-                <li>Minimize the gap between what people want vs. what they settle for</li>
-              </div>
-            </article>
-            <article className="experience__details">
-              {/* <BsPatchCheckFill className="experience__details-icon" /> */}
-              <div className="text-white">
-                <li>Standardize transfer of information between landlord and tennant   </li>
-              </div>
-            </article>
-            <article className="experience__details">
-            {/* <BsPatchCheckFill className="experience__details-icon" /> */}
-              <div className="text-white">
-              <li>Reintroduce excitement to the search process</li>
-              </div>
-            </article>
+    <div class="container experience__container reveal">
+      <div class="experience__content">
+        <article className="experience__details ">
+          {/* <BsPatchCheckFill className="experience__details-icon" /> */}
+          <div className="text-white">
+            <li>Reduce frustration.</li>
           </div>
-          
-        </div>
+        </article>
+        <article className="experience__details">
+          {/* <BsPatchCheckFill className="experience__details-icon" /> */}
+          <div className="text-white">
+            <li>
+              Minimize the gap between what people want vs. what they settle for.
+            </li>
+          </div>
+        </article>
+        <article className="experience__details">
+          {/* <BsPatchCheckFill className="experience__details-icon" /> */}
+          <div className="text-white">
+            <li>Reintroduce excitement to the search process.</li>
+
+          </div>
+        </article>
+        <article className="experience__details">
+          {/* <BsPatchCheckFill className="experience__details-icon" /> */}
+          <div className="text-white">
+            <li>Save time and energy exerted in searching.</li>
+          </div>
+        </article>
+        <article className="experience__details">
+          {/* <BsPatchCheckFill className="experience__details-icon" /> */}
+          <div className="text-white">
+            <li> Ease the competition. </li>
+          </div>
+        </article>
+        <article className="experience__details">
+          {/* <BsPatchCheckFill className="experience__details-icon" /> */}
+          <div className="text-white">
+          <li>
+              Standardize the transfer of information between landlord and
+              tenant.
+            </li>          </div>
+        </article>
+      </div>
+    </div>
   );
 };
 
